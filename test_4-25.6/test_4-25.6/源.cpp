776c4882f52fe0c88f973d48b23c8c346e796442
niuyitong <913451387@qq.com>
2019-04-25 16:47:49 +0800

    提交代码

--- test_4-25.6/test_4-25.6/源.cpp
+++ test_4-25.6/test_4-25.6/源.cpp
@@ -27,66 +27,66 @@ int main()
 }
 
 //使用函数实现两个数的交换。
-//int exchange(int *x, int *y)
-//{
-//	int t;
-//	t = *x;
-//	*x = *y;
-//	*y = t;
-//	return 1;
-//}
-//int main()
-//{
-//	int m, n;
-//	scanf("%d%d", &m, &n);
-//	int ret = exchange(&m, &n);
-//	printf("%d %d",m,n );
-//	system("pause");
-//	return 0;
-//}
+int exchange(int *x, int *y)
+{
+	int t;
+	t = *x;
+	*x = *y;
+	*y = t;
+	return 1;
+}
+int main()
+{
+	int m, n;
+	scanf("%d%d", &m, &n);
+	int ret = exchange(&m, &n);
+	printf("%d %d",m,n );
+	system("pause");
+	return 0;
+}
 
 //实现一个函数判断year是不是闰年。
-//int is_leap_year(int y)
-//{
-//	if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
-//		return 1;
-//	else
-//		return 0;
-//}
-//int main()
-//{
-//	int year = 0;
-//	scanf("%d", &year);
-//	if (is_leap_year(year))
-//		printf("是闰年");
-//	else
-//	printf("不是闰年");
-//	system("pause");
-//	return 0;
-//}
+int is_leap_year(int y)
+{
+	if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int year = 0;
+	scanf("%d", &year);
+	if (is_leap_year(year))
+		printf("是闰年");
+	else
+	printf("不是闰年");
+	system("pause");
+	return 0;
+}
 
 //实现一个函数，判断一个数是不是素数。
-//int is_prime(int n)
-//{
-//	int j;
-//	for (j = 2; j < n; j++)
-//	{
-//		if (n%j == 0)
-//			return 0;
-//	}
-//	return 1;
-//}
-//int main()
-//{
-//	int i;
-//	scanf("%d", &i);
-//	if (is_prime(i))
-//		printf("是素数");
-//	else
-//		printf("不是素数");
-//	system("pause");
-//	return 0;
-//}
+int is_prime(int n)
+{
+	int j;
+	for (j = 2; j < n; j++)
+	{
+		if (n%j == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int i;
+	scanf("%d", &i);
+	if (is_prime(i))
+		printf("是素数");
+	else
+		printf("不是素数");
+	system("pause");
+	return 0;
+}
 
 //创建一个数组，
 //实现函数init（）初始化数组、
