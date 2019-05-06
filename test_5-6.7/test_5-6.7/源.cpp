be99c0a0e35c7eccd9f197a35488074f099bf614
niuyitong <913451387@qq.com>
2019-05-07 00:01:37 +0800

    递归

--- test_5-6.7/test_5-6.7/源.cpp
+++ test_5-6.7/test_5-6.7/源.cpp
@@ -53,3 +53,125 @@ int main()
 	system("pause");
 	return 0;
 }
+
+//写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和， 
+//例如，调用DigitSum(1729)，则应该返回1 + 7 + 2 + 9，它的和是19
+int DigitSum(int x)
+{
+	if (x < 10)
+		return x;
+	else
+		return (x % 10 + DigitSum(x / 10));
+}
+
+int main()
+{
+	int n = 1729;
+	int ret = DigitSum(n);
+	printf("%d\n", ret);
+	system("pause");
+	return 0;
+}
+
+// 编写一个函数 reverse_string(char * string)（递归实现） 
+//实现：将参数字符串中的字符反向排列。
+//要求：不能使用C函数库中的字符串操作函数。
+
+void reverse_string(char *string)
+{
+	if ('\0' != *(++string))
+		reverse_string(string);
+	printf("%c", *(string - 1));
+}
+
+int main()
+{
+	char *a = "abcde";
+	reverse_string(a);
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
+//递归和非递归分别实现strlen
+//非递归
+int Strlen(char* str)
+{
+	 int i = 0;
+	 while (*str != '\0')
+	    {
+		       ++str;
+		       ++i;
+	     }
+	    return i;
+ }
+ int main()
+ {
+	     char str[] = "abcdefghh";
+	     printf("%d\n", Strlen(str));
+	     return 0;
+ }
+ //递归
+ int Strlen(char* str)
+ {
+	   if (*str == '\0')
+		   {
+		         return 0;
+		    }
+	   else
+		   {
+		        return 1 + Strlen(++str);
+		   }
+ }
+ //递归和非递归分别实现求n的阶乘
+ int main()
+	 {
+	      char str[] = "abcdefg";
+	     printf("%d\n", Strlen(str));
+	     return 0;
+	 }
+ int Factorial_r(int num)
+ {
+	 //非递归实现
+	 int k = 1;
+	 while (num > 0)
+		 k *= num--;
+	 return k;
+ }
+
+ int Factorial(int num)
+ {
+	 //递归实现
+	 return num > 0 ? num * Factorial(num - 1) : 1;
+ }
+
+ int main()
+ {
+	 int n = 0;
+
+	 printf("请输入一个数，求他的阶乘:\n");
+	 scanf("%d", &n);
+	 printf("%d\n", Factorial(n));
+	 printf("%d\n", Factorial_r(n));
+	 system("pause");
+	 return 0;
+ }
+ //递归方式实现打印一个整数的每一位 
+
+ void print(int n)
+ {
+	 if (n>9)
+	 {
+		 print(n / 10);
+	 }
+	 printf("%d  ", n % 10);
+ }
+
+ int main()
+ {
+
+	 int num = 1234;
+	 print(num);
+	 system("pause");
+	 return 0;
+ }
