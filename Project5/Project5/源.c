891ef91a034e386f36aca67d40af5a009667aeda
niuyitong <913451387@qq.com>
2019-05-13 18:29:47 +0800

    代码

---
+++ Project5/Project5/源.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//求一个整数存储在内存中的二进制中的1的个数
+//方法1
+int main()
+{
+	int num = 10;//0000 1010
+	int count = 0;
+	while (num)
+	{
+		if (1 == num % 2)
+			count++;
+		num = num / 2;
+	}
+	printf("%d\n", count);
+	system("pause");
+	return 0;
+}
