82111b5c85149c1550d7ace7b41e2d887b3af067
niuyitong <913451387@qq.com>
2019-05-13 19:02:43 +0800

    添加

--- Project5/Project5/源.c
+++ Project5/Project5/源.c
@@ -1,17 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-//求一个整数存储在内存中的二进制中的1的个数
-//方法1
+////求一个整数存储在内存中的二进制中的1的个数
+////方法1
+//int main()
+//{
+//	int num = 10;//0000 1010
+//	int count = 0;
+//	while (num)
+//	{
+//		if (1 == num % 2)
+//			count++;
+//		num = num / 2;
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}
+
+////方法二
+//int main()
+//{
+//	int num = -1;
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((num >> i) & 1 == 1)
+//			count++;
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}
+
+//方法3
 int main()
 {
-	int num = 10;//0000 1010
+	int num = -1;
+	int i = 0;
 	int count = 0;
 	while (num)
 	{
-		if (1 == num % 2)
-			count++;
-		num = num / 2;
+		count++;
+		num = num&(num - 1);
 	}
 	printf("%d\n", count);
 	system("pause");
